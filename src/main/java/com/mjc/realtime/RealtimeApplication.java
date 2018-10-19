9c79f6cbec125e5931c4560430615214ef6a0567
MaJunChaoGit <354488954@qq.com>
2018-10-19 17:26:11 +0800

    没有link容器导致的路由 搜索问题

--- src/main/java/com/mjc/realtime/RealtimeApplication.java
+++ src/main/java/com/mjc/realtime/RealtimeApplication.java
@@ -3,12 +3,24 @@ package com.mjc.realtime;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @MapperScan("com.mjc.realtime.mapper")
-public class RealtimeApplication {
-
+public class RealtimeApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(RealtimeApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RealtimeApplication.class, args);
 	}
+
+
 }
